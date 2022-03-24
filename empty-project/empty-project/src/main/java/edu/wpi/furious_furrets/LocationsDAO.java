8984a867e70781cecb77ef34b317e993fd151302
Owen Radcliffe <62451536+radcliffeowe@users.noreply.github.com>
2022-03-24 09:39:04 -0400

    Command line interface for selecting option, methods prototyped, no functionality yet.

--- empty-project/empty-project/src/main/java/edu/wpi/furious_furrets/LocationsDAO.java
+++ empty-project/empty-project/src/main/java/edu/wpi/furious_furrets/LocationsDAO.java
@@ -1,10 +1,13 @@
 package edu.wpi.furious_furrets;
 
+import java.io.*;
 import java.sql.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class LocationsDAO {
 
-  public void testConnection() throws SQLException {
+  public void testConnection() throws SQLException, NoSuchElementException {
 
     System.out.println("Database Setup");
 
@@ -19,7 +22,47 @@ public class LocationsDAO {
     Connection connection = null;
 
     try {
-      connection = DriverManager.getConnection("jdbc:derby:C:/Users/radcl/Locations");
+      connection = DriverManager.getConnection("jdbc:derby:C:/Users/radcl/DB;create=true");
+      assert (connection != null);
+      System.out.println("---------------- User Menu ------------------------");
+      System.out.println(
+          "Please Enter A Number:\n"
+              + "1- Location Information\n"
+              + "2- Change Floor and Type\n"
+              + "3- Enter Location\n"
+              + "4- Delete Location\n"
+              + "5- Save Locations to CSV File\n"
+              + "6- Exit Program");
+
+      Scanner inp = new Scanner(System.in);
+      int option = 0;
+      while (option != 6) {
+        System.out.println("Option:");
+        option = Integer.parseInt(inp.nextLine());
+        switch (option) {
+          case 1:
+            displayLocInfo();
+            break;
+          case 2:
+            updateLocation();
+            break;
+          case 3:
+            insertLocation();
+            break;
+          case 4:
+            deleteLocation();
+            break;
+          case 5:
+            saveLocation();
+            break;
+          case 6:
+            System.out.println("Exiting program...");
+            return;
+          default:
+            System.out.println("Please enter an option 1-6");
+            break;
+        }
+      }
       Statement stm = connection.createStatement();
       // stm.execute("INSERT into Students (sid, name, grade, classnum) values (1, 'Owen Radcliffe',
       // 11, 13)");
@@ -37,9 +80,58 @@ public class LocationsDAO {
       System.out.println("Connection failed");
       e.printStackTrace();
       return;
+    } catch (NoSuchElementException e) {
+      System.out.println("No line found");
+      e.printStackTrace();
+      return;
     }
 
     System.out.println("Derby connection established");
     connection.close();
+  }
+
+  /*
+   Option 1, The program displays the list of location nodes along with their attributes.
+  Then the menu is displayed again and the user is prompted for the next selection.
+   */
+  private ResultSet displayLocInfo() {
+    System.out.println("Displaying Location Information...");
+    return null;
+  }
+
+  /*
+  Option 2, The user is prompted for the ID of the location node and then is prompted for
+  the new values of the floor and location type. Then the menu is displayed again and
+  the user is prompted for the next selection.
+   */
+  private void updateLocation() {
+    System.out.println("Updating Location Information...");
+  }
+
+  /*
+  Option 3, The user is prompted for the ID of the new location node. A new Java Location
+  object is created and the node is added to the SQL table. Then the menu is displayed
+   again and the user prompted for the next selection.
+   */
+  private void insertLocation() {
+    System.out.println("Adding New Location...");
+  }
+
+  /*
+   Option 4, The user is prompted for the ID of the location node. The node is removed from
+   the SQL table, and the corresponding Java object is deleted. Then the menu is displayed
+   again and the user prompted for the next selection.
+  */
+  private void deleteLocation() {
+    System.out.println("Deleting Location...");
+  }
+
+  /*
+  Option 5, The user is prompted for the name of the CSV file. The program first loads all of the
+  contents of the SQL Location table into Java Location objects. Then the CSV file is
+  created from the Java objects.
+   */
+  private void saveLocation() {
+    System.out.println("Saving Location Information...");
   }
 }
--- empty-project/empty-project/src/main/java/edu/wpi/furious_furrets/Main.java
+++ empty-project/empty-project/src/main/java/edu/wpi/furious_furrets/Main.java
@@ -1,10 +1,11 @@
 package edu.wpi.furious_furrets;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 public class Main {
 
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) throws SQLException, NoSuchElementException {
 
     LocationsDAO ldao = new LocationsDAO();
     ldao.testConnection();
