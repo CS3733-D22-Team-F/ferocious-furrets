4746deea68a4cc2b92562e911d630669f6ee2d7e
Azu109 <deshiming@outlook.com>
2022-03-24 19:57:39 -0400

    V1 HomePage

--- empty-project/empty-project/src/main/java/edu/wpi/teamname/App.java
+++ empty-project/empty-project/src/main/java/edu/wpi/teamname/App.java
@@ -1,6 +1,10 @@
 package edu.wpi.teamname;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +17,16 @@ public class App extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {}
+  public void start(Stage primaryStage) throws IOException {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+      Scene scene = new Scene(root, 1280, 720);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   @Override
   public void stop() {
---
+++ empty-project/empty-project/src/main/java/edu/wpi/teamname/homePageController.java
@@ -0,0 +1,11 @@
+package edu.wpi.teamname;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class homePageController implements Initializable {
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+}
