c560a0a0833540ae1652f062479a7e518fe077f3
Azu109 <deshiming@outlook.com>
2022-03-30 13:23:00 -0400

    Changes

--- Fapp/src/main/java/edu/wpi/teamname/logInController.java
+++ Fapp/src/main/java/edu/wpi/teamname/logInController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class logInController extends returnHomePage {
+public class logInController extends menu {
   @FXML private TextField usernameField;
   @FXML private TextField passwordField;
   @FXML private Label popUpLabel;
