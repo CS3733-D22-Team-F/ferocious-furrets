4bc44d5258c9cbb32536400ae0ed923063bad7cd
Conner McKevitt <cmmckevitt@wpi.edu>
2022-04-04 21:56:11 -0400

    comments

--- ProtoypeF/src/main/java/edu/wpi/furious_furrets/database/DatabaseInitializer.java
+++ ProtoypeF/src/main/java/edu/wpi/furious_furrets/database/DatabaseInitializer.java
@@ -1,64 +1,72 @@
 package edu.wpi.furious_furrets.database;
 
-import java.sql.*;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/** Class for handling the connection to the DB, ensuring we only have on connection from the app */
+/**
+ * Class for handling the connection to the DB, ensuring we only have on connection from the app
+ */
 public class DatabaseInitializer {
 
-  public Connection dbConnection;
+    public Connection dbConnection;
 
-  private DatabaseInitializer() {
-    this.dbConnection = this.connectDatabase();
-  }
-
-  private static class Helper {
-    private static final DatabaseInitializer db = new DatabaseInitializer();
-  }
-
-  public static DatabaseInitializer getConnection() {
-    return Helper.db;
-  }
-
-  /** */
-  //  public DatabaseInitializer() {
-  //    this.dbConnection = this.connectDatabase();
-  //  }
-
-  /**
-   * Connects to the database returns null object if connection failed
-   *
-   * @return Connection object
-   */
-  public Connection connectDatabase() {
-    try {
-      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-    } catch (ClassNotFoundException e) {
-      System.out.println("Driver not found");
-      e.printStackTrace();
+    /**
+     * Constructor
+     */
+    private DatabaseInitializer() {
+        this.dbConnection = this.connectDatabase();
     }
 
-    System.out.println("Driver registered");
-    dbConnection = null;
-
-    try {
-      dbConnection =
-          DriverManager.getConnection("jdbc:derby:myDB;create=true"); // CONNECT TO DATABASE
-      assert (dbConnection != null);
-      // initTable();
-
-    } catch (SQLException e) {
-      System.out.println("Connection failed");
-      e.printStackTrace();
-      return null;
+    /**
+     * returns a DatabaseInitializer object from the Helper in DatabaseManager
+     *
+     * @return DatabaseInitializer
+     */
+    public static DatabaseInitializer getConnection() {
+        return Helper.db;
     }
 
-    System.out.println("Derby connection established");
-    return dbConnection;
-  }
+    /**
+     * Connects to the database returns null object if connection failed
+     *
+     * @return Connection object
+     */
+    public Connection connectDatabase() {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found");
+            e.printStackTrace();
+        }
 
-  public Connection getDbConnection() {
-    return dbConnection;
-  }
+        System.out.println("Driver registered");
+        dbConnection = null;
+
+        try {
+            dbConnection =
+                    DriverManager.getConnection("jdbc:derby:myDB;create=true"); // CONNECT TO DATABASE
+            assert (dbConnection != null);
+            // initTable();
+
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+            return null;
+        }
+
+        System.out.println("Derby connection established");
+        return dbConnection;
+    }
+
+    /**
+     * @return
+     */
+    public Connection getDbConnection() {
+        return dbConnection;
+    }
+
+    private static class Helper {
+        private static final DatabaseInitializer db = new DatabaseInitializer();
+    }
 }
