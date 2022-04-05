71108793c34941318fd2f72f35d69f6a043337a5
Owen Radcliffe <62451536+radcliffeowe@users.noreply.github.com>
2022-04-04 23:04:21 -0400

    Added File Explorer window to load/reset CSV functionality for UX

--- ProtoypeF/src/main/java/edu/wpi/furious_furrets/controllers/scene_application/ServiceRequestForm.java
+++ ProtoypeF/src/main/java/edu/wpi/furious_furrets/controllers/scene_application/ServiceRequestForm.java
@@ -1,25 +1,14 @@
 package edu.wpi.furious_furrets.controllers.scene_application;
 
-/**
- * Abstract for a service request for class
- */
+/** Abstract for a service request for class */
 public abstract class ServiceRequestForm {
 
-    /**
-     * Assign an employee to a service request
-     */
-    public void assignName() {
-    }
+  /** Assign an employee to a service request */
+  public void assignName() {}
 
-    /**
-     * Submits request to be saved in the database
-     */
-    public void submit() {
-    }
+  /** Submits request to be saved in the database */
+  public void submit() {}
 
-    /**
-     * Clears text fields in form
-     */
-    public void reset() {
-    }
+  /** Clears text fields in form */
+  public void reset() {}
 }
--- ProtoypeF/src/main/java/edu/wpi/furious_furrets/database/DatabaseInitializer.java
+++ ProtoypeF/src/main/java/edu/wpi/furious_furrets/database/DatabaseInitializer.java
@@ -4,69 +4,63 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Class for handling the connection to the DB, ensuring we only have on connection from the app
- */
+/** Class for handling the connection to the DB, ensuring we only have on connection from the app */
 public class DatabaseInitializer {
 
-    public Connection dbConnection;
+  public Connection dbConnection;
 
-    /**
-     * Constructor
-     */
-    private DatabaseInitializer() {
-        this.dbConnection = this.connectDatabase();
+  /** Constructor */
+  private DatabaseInitializer() {
+    this.dbConnection = this.connectDatabase();
+  }
+
+  /**
+   * returns a DatabaseInitializer object from the Helper in DatabaseManager
+   *
+   * @return DatabaseInitializer
+   */
+  public static DatabaseInitializer getConnection() {
+    return Helper.db;
+  }
+
+  /**
+   * Connects to the database returns null object if connection failed
+   *
+   * @return Connection object
+   */
+  public Connection connectDatabase() {
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Driver not found");
+      e.printStackTrace();
     }
 
-    /**
-     * returns a DatabaseInitializer object from the Helper in DatabaseManager
-     *
-     * @return DatabaseInitializer
-     */
-    public static DatabaseInitializer getConnection() {
-        return Helper.db;
+    System.out.println("Driver registered");
+    dbConnection = null;
+
+    try {
+      dbConnection =
+          DriverManager.getConnection("jdbc:derby:myDB;create=true"); // CONNECT TO DATABASE
+      assert (dbConnection != null);
+      // initTable();
+
+    } catch (SQLException e) {
+      System.out.println("Connection failed");
+      e.printStackTrace();
+      return null;
     }
 
-    /**
-     * Connects to the database returns null object if connection failed
-     *
-     * @return Connection object
-     */
-    public Connection connectDatabase() {
-        try {
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver not found");
-            e.printStackTrace();
-        }
+    System.out.println("Derby connection established");
+    return dbConnection;
+  }
 
-        System.out.println("Driver registered");
-        dbConnection = null;
+  /** @return */
+  public Connection getDbConnection() {
+    return dbConnection;
+  }
 
-        try {
-            dbConnection =
-                    DriverManager.getConnection("jdbc:derby:myDB;create=true"); // CONNECT TO DATABASE
-            assert (dbConnection != null);
-            // initTable();
-
-        } catch (SQLException e) {
-            System.out.println("Connection failed");
-            e.printStackTrace();
-            return null;
-        }
-
-        System.out.println("Derby connection established");
-        return dbConnection;
-    }
-
-    /**
-     * @return
-     */
-    public Connection getDbConnection() {
-        return dbConnection;
-    }
-
-    private static class Helper {
-        private static final DatabaseInitializer db = new DatabaseInitializer();
-    }
+  private static class Helper {
+    private static final DatabaseInitializer db = new DatabaseInitializer();
+  }
 }
