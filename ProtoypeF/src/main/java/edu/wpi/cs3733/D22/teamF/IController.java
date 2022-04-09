65fadf6fcfff10f8e63a89a6763568a0eeeb7be0
Evans Owusu <eaowusu@wpi.edu>
2022-04-08 21:57:48 -0400

    Page Controller & IController
    Page Controller
    To be extended to all pages
    
    IController Interface for pages

---
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/IController.java
@@ -0,0 +1,26 @@
+package edu.wpi.cs3733.D22.teamF;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuBar;
+
+// Interface for all pages
+public interface IController extends Initializable {
+
+    /**
+     * clear user inputed fields of a page
+     */
+    public void clear();
+
+    /**
+     * Makes a context menu for a specific page
+     * @return a context menu
+     */
+    public ContextMenu makeContextMenu();
+
+    /**
+     *  Makes a menu bar
+     * @return a menu bar specific to the page
+     */
+    public MenuBar makeMenuBar();
+}
