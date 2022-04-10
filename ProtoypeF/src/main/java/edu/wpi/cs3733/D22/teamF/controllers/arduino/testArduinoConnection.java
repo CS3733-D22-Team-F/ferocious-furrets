8af59cb42f957f724097ffa13ee936c8a3df30c0
cfp02 <47919940+cfp02@users.noreply.github.com>
2022-04-10 19:44:45 -0400

    Started imports

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/arduino/testArduinoConnection.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/arduino/testArduinoConnection.java
@@ -1,23 +1,9 @@
 package edu.wpi.cs3733.D22.teamF.controllers.arduino;
-// package edu.wpi.cs3733.D22.teamF.libs.ArduinoCom;
-// import arduino.*;
+
+import com.fazecast.jSerialComm.*;
 
 public class testArduinoConnection {
 
-  //    Arduino arduino;
-  //
-  //    public String readData() {
-  //        String input = "";
-  //        arduino = new Arduino("COM4", 9600); //this is where you specify your COM
-  //        boolean connection = arduino.openConnection();
-  //        if (!connection) {
-  //            System.err.println("Error: Unable to Connect to serial port");
-  //        } else {
-  //            while (input.equals("")) {
-  //                input = arduino.serialRead();
-  //            }
-  //        }
-  //        System.out.println(input);
-  //        return input;
-  //    }
+  SerialPort ports[] = SerialPort.getCommPorts();
+  SerialPort sp = ports[0];
 }
