86b16bb349932827cde28cd647ea03869ef427cb
cfp02 <47919940+cfp02@users.noreply.github.com>
2022-04-10 18:44:10 -0400

    Testing arduino implementation

---
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/arduino/testArduinoConnection.java
@@ -0,0 +1,23 @@
+package edu.wpi.cs3733.D22.teamF.controllers.arduino;
+// package edu.wpi.cs3733.D22.teamF.libs.ArduinoCom;
+// import arduino.*;
+
+public class testArduinoConnection {
+
+  //    Arduino arduino;
+  //
+  //    public String readData() {
+  //        String input = "";
+  //        arduino = new Arduino("COM4", 9600); //this is where you specify your COM
+  //        boolean connection = arduino.openConnection();
+  //        if (!connection) {
+  //            System.err.println("Error: Unable to Connect to serial port");
+  //        } else {
+  //            while (input.equals("")) {
+  //                input = arduino.serialRead();
+  //            }
+  //        }
+  //        System.out.println(input);
+  //        return input;
+  //    }
+}
