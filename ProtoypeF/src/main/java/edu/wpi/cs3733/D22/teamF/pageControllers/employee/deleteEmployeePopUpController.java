d1248c128b3113db3562217fe53a515970438280
Will Huang <whuang2146@gmail.com>
2022-04-12 23:15:22 -0400

    add and modify pages updated

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/employee/deleteEmployeePopUpController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/employee/deleteEmployeePopUpController.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.D22.teamF.pageControllers.employee;
 
+import edu.wpi.cs3733.D22.teamF.controllers.fxml.StageManager;
 import edu.wpi.cs3733.D22.teamF.controllers.general.DatabaseManager;
 import edu.wpi.cs3733.D22.teamF.pageControllers.PageController;
 import java.net.URL;
@@ -9,14 +10,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ContextMenu;
 
 public class deleteEmployeePopUpController extends PageController implements Initializable {
+
+
   public void submit() throws SQLException {
     String employeeID = null;
     DatabaseManager.getEmployeeDAO().delete(employeeID);
   }
 
-  public void reset() {}
+  public void reset() {
 
-  public void back() {}
+  }
+
+  public void back() {
+    StageManager.getInstance().setDisplay("employee/employeePage.fxml");
+  }
 
   /**
    * Method to create a class specifics context menu
