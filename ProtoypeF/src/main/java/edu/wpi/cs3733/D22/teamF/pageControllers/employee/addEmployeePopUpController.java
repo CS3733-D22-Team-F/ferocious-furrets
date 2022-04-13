1700fe664e1ddd6932ad9a5dc402064001346f0d
John Petrarca <64863242+johnmaxpetrarca@users.noreply.github.com>
2022-04-12 22:46:33 -0400

    addemployee fxml and controller updates john

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/employee/addEmployeePopUpController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/employee/addEmployeePopUpController.java
@@ -1,18 +1,32 @@
 package edu.wpi.cs3733.D22.teamF.pageControllers.employee;
 
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.D22.teamF.controllers.fxml.StageManager;
 import edu.wpi.cs3733.D22.teamF.controllers.requests.IRequestController;
 import edu.wpi.cs3733.D22.teamF.pageControllers.PageController;
+
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ContextMenu;
 
 public class addEmployeePopUpController extends PageController
     implements Initializable, IRequestController {
+
+  @FXML
+  JFXButton backButton;
   public void submit() throws SQLException {}
 
   public void reset() {}
+
+  public void back(ActionEvent event){
+    StageManager.getInstance().setDisplay("employee/employeePage.fxml");
+
+  }
 
   /**
    * Method to create a class specifics context menu
@@ -31,4 +45,8 @@ public class addEmployeePopUpController extends PageController
    * @param resources The resources used to localize the root object, or {@code null} if
    */
   public void initialize(URL location, ResourceBundle resources) {}
+
+  public void back(javafx.event.ActionEvent actionEvent) {
+    StageManager.getInstance().setDisplay("employee/employeePage.fxml");
+  }
 }
