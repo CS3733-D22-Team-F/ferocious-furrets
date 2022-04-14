6ff1d186867de48984280880a1893348f7a1b6db
Owen Radcliffe <62451536+radcliffeowe@users.noreply.github.com>
2022-04-14 10:38:56 -0400

    Updated loading screen to display thank you message to staff user

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/cachePageController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/cachePageController.java
@@ -7,15 +7,22 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.concurrent.Task;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class cachePageController implements Initializable {
 
+  @FXML Label statusLabel;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    statusLabel.setText(randomFact());
+    statusLabel.setTextAlignment(TextAlignment.CENTER);
     Task<Void> loader = new Load();
     loader.setOnSucceeded(
         e -> {
@@ -31,5 +38,52 @@ public class cachePageController implements Initializable {
         });
     Thread thread = new Thread(loader, "Starting App...");
     thread.start();
+  }
+
+  private String randomFact() {
+    String fact = "";
+    double rand = Math.random();
+    double whichFact = rand * 10;
+    int mod = (int) whichFact;
+    switch (mod) {
+      case 0:
+        fact =
+            "Thank YOU for making Brigham & Women's Hospital \na leader in the healthcare industry!";
+        break;
+      case 1:
+        fact =
+            "Thank YOU for helping us provide leading healthcare \nto thousands of patients annually!";
+        break;
+      case 2:
+        fact = "Thank YOU, we wouldn't be the same without you!";
+        break;
+      case 3:
+        fact = "Thank YOU for your tireless work to improve our patient's lives!";
+        break;
+      case 4:
+        fact = "Improving our patient's lives since 1980!";
+        break;
+      case 5:
+        fact =
+            "Thank YOU for making Brigham & Women's Hospital a \nleader in the healthcare industry!";
+        break;
+      case 6:
+        fact =
+            "Thank YOU for helping us provide leading healthcare \nto thousands of patients annually!";
+        break;
+      case 7:
+        fact = "Thank YOU, we wouldn't be the same without you!";
+        break;
+      case 8:
+        fact = "Thank YOU for your tireless work to improve our patient's lives!";
+        break;
+      case 9:
+        fact =
+            "Thank YOU for your tireless work! Every one of you is an \nessential part of this hospital!";
+        break;
+      default:
+        break;
+    }
+    return fact;
   }
 }
