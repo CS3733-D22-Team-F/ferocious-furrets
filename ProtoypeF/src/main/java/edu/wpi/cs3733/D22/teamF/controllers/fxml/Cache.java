b6f54792978a4b33b4db1a923528e1b52ea45814
Totally Azu <sde@wpi.edu>
2022-04-13 22:43:00 -0400

    Added location datacache

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/fxml/Cache.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/fxml/Cache.java
@@ -6,6 +6,7 @@ import edu.wpi.cs3733.D22.teamF.Fapp;
 import edu.wpi.cs3733.D22.teamF.Map.MapComponents.MapIconModifier;
 import edu.wpi.cs3733.D22.teamF.controllers.general.DatabaseManager;
 import edu.wpi.cs3733.D22.teamF.entities.database.DatabaseInitializer;
+import edu.wpi.cs3733.D22.teamF.entities.location.Location;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
@@ -16,7 +17,14 @@ import org.reflections.scanners.Scanners;
 public class Cache {
   // load everything
 
-  public static boolean isDataUpdated = false;
+  private static boolean isLocationsUpdated = false;
+  private static ArrayList<Location> locationsCache = new ArrayList<>();
+  private static HashMap<String, Location> locationsCacheMap = new HashMap<>();
+
+  public enum DBType {
+    DBT_ALL,
+    DBT_LOC
+  }
 
   public static void loadViews() throws SQLException, IOException {
     DatabaseManager.switchConnection(DatabaseInitializer.ConnType.EMBEDDED);
@@ -41,8 +49,75 @@ public class Cache {
     }
   }
 
-  public static void loadIcons() throws IOException {
+  public static void loadIcons(){
     InputStream file = Fapp.class.getResourceAsStream("Map/Icons/MapMenuSVG/home.svg");
     MapIconModifier.iconLoader.loadSvg(file);
+  }
+
+  /*
+     Database Cache Setters/Getters
+  */
+  public static ArrayList<Location> getLocationsCache() {
+    if (isLocationsUpdated) {
+      try {
+        updateDBCache(DBType.DBT_LOC);
+      } catch (Exception e) {
+        System.out.println("ERROR! Couldn't update locations!");
+        e.printStackTrace();
+        return null;
+      }
+    }
+    return locationsCache;
+  }
+
+  public static Location getLocation(String nodeID) {
+    if (isLocationsUpdated) {
+      try {
+        updateDBCache(DBType.DBT_LOC);
+      } catch (Exception e) {
+        System.out.println("ERROR! Couldn't update locations!");
+        e.printStackTrace();
+        return null;
+      }
+    }
+    return locationsCacheMap.get(nodeID);
+  }
+
+  public static void updateDBCache() throws SQLException {
+    updateDBCache(DBType.DBT_ALL);
+  }
+
+  /*
+     Database Cache Management
+  */
+  public static void updateDBCache(DBType type) throws SQLException {
+    switch (type) {
+      case DBT_LOC:
+        locationsCache = DatabaseManager.getLocationDAO().getAllLocations();
+        isLocationsUpdated = true;
+        buildMaps(DBType.DBT_LOC);
+      case DBT_ALL:
+      default:
+        locationsCache = DatabaseManager.getLocationDAO().getAllLocations();
+        isLocationsUpdated = true;
+        buildMaps(DBType.DBT_ALL);
+    }
+    System.out.println("Updated Cache");
+  }
+
+  private static void buildMaps(DBType type) {
+    switch (type) {
+      case DBT_LOC:
+        locationsCacheMap = new HashMap<>();
+        for (Location loc : locationsCache) {
+          locationsCacheMap.put(loc.getNodeID(), loc);
+        }
+      case DBT_ALL:
+      default:
+        locationsCacheMap = new HashMap<>();
+        for (Location loc : locationsCache) {
+          locationsCacheMap.put(loc.getNodeID(), loc);
+        }
+    }
   }
 }
--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/fxml/Load.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/controllers/fxml/Load.java
@@ -8,6 +8,7 @@ public class Load extends Task<Void> {
 
   @Override
   protected Void call() throws IOException, SQLException {
+    Cache.updateDBCache();
     Cache.loadViews();
     Cache.loadIcons();
     return null;
