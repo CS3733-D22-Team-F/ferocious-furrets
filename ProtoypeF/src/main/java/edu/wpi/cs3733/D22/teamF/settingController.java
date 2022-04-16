7765f72a37218803d0fca402b70bafb883b01d4f
ngrozdani <nikola_grozdani1@yahoo.com>
2022-04-15 22:32:03 -0400

    credits page & controller

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/settingController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/settingController.java
@@ -6,16 +6,21 @@ import edu.wpi.cs3733.D22.teamF.controllers.fxml.SceneManager;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class settingController implements Initializable {
   @FXML private Label themeSetLabel;
   @FXML private JFXComboBox themeSetCombo;
+  @FXML private ImageView furretImage;
   @FXML private JFXButton logoutButton;
 
   @FXML
@@ -33,9 +38,22 @@ public class settingController implements Initializable {
     stage.show();
   }
 
+  @FXML
+  public void furretCredit(){
+      
+  }
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     themeSetLabel.setVisible(false);
     themeSetCombo.setVisible(false);
+    furretImage.setPickOnBounds(true);
+
+    furretImage.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+      @Override
+      public void handle(MouseEvent event) {
+
+      }
+    });
   }
 }
