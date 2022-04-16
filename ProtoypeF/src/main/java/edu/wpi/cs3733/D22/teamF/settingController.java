4d952fd3f0b52f24812135f051e8ebb340363ba8
ngrozdani <nikola_grozdani1@yahoo.com>
2022-04-15 21:54:18 -0400

    controller for setting fxml

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/settingController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/settingController.java
@@ -1,3 +1,16 @@
 package edu.wpi.cs3733.D22.teamF;
 
-public class settingController {}
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class settingController {
+    @FXML
+    private Label themeSetLabel;
+    @FXML
+    private JFXComboBox themeSetCombo;
+
+
+
+
+}
