790e3ccee66d1045c30955d68d3a9c7a043fa3ce
Azu109 <deshiming@outlook.com>
2022-04-19 17:59:54 -0400

    Pop up modify request and get selected item

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/requestListController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/pageControllers/requestListController.java
@@ -1,16 +1,31 @@
 package edu.wpi.cs3733.D22.teamF.pageControllers;
 
+import edu.wpi.cs3733.D22.teamF.Fapp;
+import edu.wpi.cs3733.D22.teamF.Map.MapComponents.locTempHolder;
+import edu.wpi.cs3733.D22.teamF.controllers.general.DatabaseManager;
+import edu.wpi.cs3733.D22.teamF.entities.request.Request;
+import edu.wpi.cs3733.D22.teamF.entities.request.deliveryRequest.equipmentDeliveryRequest;
 import edu.wpi.cs3733.D22.teamF.serviceRequestStorage;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
+
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.ListView;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * controller for request list
@@ -19,6 +34,9 @@ import javafx.scene.layout.AnchorPane;
  */
 public class requestListController extends PageController implements Initializable {
 
+
+  public static String selectedID = "";
+  public static String selectedType = "";
   @FXML ListView requestList;
   @FXML private AnchorPane masterPane;
 
@@ -51,5 +69,30 @@ public class requestListController extends PageController implements Initializab
   @FXML
   void switchToHome(ActionEvent event) throws IOException {
     // StageManager.getInstance().setHome();
+  }
+
+  void onChangePress() throws IOException {
+    //TODO replace "new" items with defined items
+    TreeTableView<Request> table = new TreeTableView<>();
+    TreeItem<Request> selectedItem = table.getSelectionModel().getSelectedItem();
+    selectedID = selectedItem.getValue().getReqID();
+    selectedType = selectedItem.getValue().getReqType();
+    popUpModifyReq();
+    //requestListController.selectedID;
+    //requestListController.selectedType;
+  }
+
+
+
+  void popUpModifyReq() throws IOException {
+    Parent root =
+            FXMLLoader.load(
+                    Objects.requireNonNull(Fapp.class.getResource("Map/mapEquipModifyPage.fxml")));
+    Stage popupwindow = new Stage();
+    popupwindow.initModality(Modality.APPLICATION_MODAL);
+    Scene scene1 = new Scene(root);
+    popupwindow.setScene(scene1);
+    popupwindow.initModality(Modality.APPLICATION_MODAL);
+    popupwindow.showAndWait();
   }
 }
