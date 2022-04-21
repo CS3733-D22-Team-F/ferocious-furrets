60ad62f5b48defff9c98f706def5c7894c36fac5
ngrozdani <64702995+ngrozdani@users.noreply.github.com>
2022-04-21 16:27:53 -0400

    raffi fxml popup

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/aboutPageController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/aboutPageController.java
@@ -1,14 +1,23 @@
 package edu.wpi.cs3733.D22.teamF;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class aboutPageController implements Initializable {
 
@@ -18,34 +27,70 @@ public class aboutPageController implements Initializable {
   @FXML Label getNickLabel2;
   @FXML AnchorPane bannerAnchor;
 
-  @FXML
-  ImageView raffi;
-  @FXML
-  ImageView carter;
-  @FXML
-  ImageView nikola;
-  @FXML
-  ImageView jack;
-  @FXML
-  ImageView will;
-  @FXML
-  ImageView connor;
-  @FXML
-  ImageView evans;
-  @FXML
-  ImageView cole;
-  @FXML
-  ImageView john;
-  @FXML
-  ImageView owen;
-  @FXML
-  ImageView azu;
+  @FXML ImageView raffi;
+  @FXML ImageView carter;
+  @FXML ImageView nikola;
+  @FXML ImageView jack;
+  @FXML ImageView will;
+  @FXML ImageView connor;
+  @FXML ImageView evans;
+  @FXML ImageView cole;
+  @FXML ImageView john;
+  @FXML ImageView owen;
+  @FXML ImageView azu;
 
+  public void changeTo(String path) throws IOException {
+    Stage stage = new Stage();
+    Scene root = FXMLLoader.load(Objects.requireNonNull(Fapp.class.getResource(path)));
+    stage.setScene(root);
+    stage.show();
+  }
+
+  @FXML
+  public void raffiCredit() {
+    raffi.setOnMouseClicked(
+        new EventHandler<MouseEvent>() {
+          @Override
+          public void handle(MouseEvent event) {
+
+            Parent root = null;
+            try {
+              root =
+                  FXMLLoader.load(
+                      Objects.requireNonNull(
+                          Fapp.class.getResource("views/teammembers/raffi.fxml")));
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+            Stage popupwindow = new Stage();
+            popupwindow.initModality(Modality.APPLICATION_MODAL);
+            Scene scene1 = new Scene(root);
+            popupwindow.setScene(scene1);
+            popupwindow.initModality(Modality.APPLICATION_MODAL);
+            popupwindow.showAndWait();
+
+            //            FXMLLoader fxmlLoader =
+            //                new
+            // FXMLLoader(Fapp.class.getResource("views/teammembers/raffi.fxml"));
+            //            Scene scene = null;
+            //            try {
+            //              scene = new Scene(fxmlLoader.load());
+            //            } catch (IOException e) {
+            //              e.printStackTrace();
+            //            }
+            //            Stage stage = SceneManager.getInstance().getStage();
+            //            SceneManager.getInstance().setStage(stage);
+            //            stage.setScene(scene);
+            //            stage.show();
+          }
+        });
+  }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    raffi.setPickOnBounds(true);
 
-    bannerAnchor.maxWidthProperty().bind(creditPane.maxHeightProperty());
+    //    bannerAnchor.maxWidthProperty().bind(creditPane.maxHeightProperty());
     //    creditVbox.layoutXProperty().bind(creditPane.maxWidthProperty());
     //    creditVbox.layoutYProperty().bind(creditPane.maxHeightProperty());
     //    creditVbox.maxHeightProperty().bind(creditVbox.heightProperty());
---
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/raffiController.java
@@ -0,0 +1,7 @@
+package edu.wpi.cs3733.D22.teamF;
+
+import javafx.scene.input.MouseEvent;
+
+public class raffiController {
+  public void raffiCredit(MouseEvent mouseEvent) {}
+}
