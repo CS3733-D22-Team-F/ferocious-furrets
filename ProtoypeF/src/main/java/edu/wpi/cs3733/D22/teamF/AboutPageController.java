8520705577d41eceb4f2f049d27cab48598ed3fa
ngrozdani <64702995+ngrozdani@users.noreply.github.com>
2022-04-21 17:14:22 -0400

    cole popup

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/AboutPageController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/AboutPageController.java
@@ -218,7 +218,7 @@ public class AboutPageController implements Initializable {
 
   @FXML
   public void evansCredit() {
-    connor.setOnMouseClicked(
+    evans.setOnMouseClicked(
             new EventHandler<MouseEvent>() {
               @Override
               public void handle(MouseEvent event) {
@@ -229,6 +229,32 @@ public class AboutPageController implements Initializable {
                           FXMLLoader.load(
                                   Objects.requireNonNull(
                                           Fapp.class.getResource("views/teammembers/evans.fxml")));
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+                Stage popupwindow = new Stage();
+                popupwindow.initModality(Modality.APPLICATION_MODAL);
+                Scene scene1 = new Scene(root);
+                popupwindow.setScene(scene1);
+                popupwindow.initModality(Modality.APPLICATION_MODAL);
+                popupwindow.showAndWait();
+              }
+            });
+  }
+
+  @FXML
+  public void coleCredit() {
+    cole.setOnMouseClicked(
+            new EventHandler<MouseEvent>() {
+              @Override
+              public void handle(MouseEvent event) {
+
+                Parent root = null;
+                try {
+                  root =
+                          FXMLLoader.load(
+                                  Objects.requireNonNull(
+                                          Fapp.class.getResource("views/teammembers/cole.fxml")));
                 } catch (IOException e) {
                   e.printStackTrace();
                 }
