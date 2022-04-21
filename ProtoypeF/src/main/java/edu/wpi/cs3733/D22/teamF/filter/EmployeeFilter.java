34def8d0aea2bff5f72af202c4ba32b462cf09df
John Petrarca <64863242+johnmaxpetrarca@users.noreply.github.com>
2022-04-21 18:28:15 -0400

    Employee Filter, Request Filter classes made

---
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/filter/EmployeeFilter.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs3733.D22.teamF.filter;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class EmployeeFilter implements IFilter{
+
+    public EmployeeFilter(String filterBy){
+
+    }
+
+    @Override
+    public ArrayList<Object> apply(ResultSet rSet) {
+        return null;
+    }
+}
---
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/filter/RequestFilter.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs3733.D22.teamF.filter;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class RequestFilter implements IFilter{
+
+    public RequestFilter(String filterBy){
+
+    }
+
+    @Override
+    public ArrayList<Object> apply(ResultSet rSet) {
+        return null;
+    }
+}
