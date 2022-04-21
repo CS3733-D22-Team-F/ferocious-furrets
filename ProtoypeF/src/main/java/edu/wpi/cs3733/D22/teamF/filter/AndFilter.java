fbafb0dfd50ed3f29c8c1bb6d2f4c21d79d5576a
Evans Owusu <eaowusu@wpi.edu>
2022-04-21 19:55:33 -0400

    AndFilter

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/filter/AndFilter.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/filter/AndFilter.java
@@ -3,17 +3,17 @@ package edu.wpi.cs3733.D22.teamF.filter;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class AndFilter implements IFilter{
-    private IFilter filter;
-    private IFilter filter2;
+public class AndFilter implements IFilter {
+  private IFilter filter;
+  private IFilter filter2;
 
-    public AndFilter(IFilter filter, IFilter filter2 ){
-        this.filter = filter;
-        this.filter2 = filter2;
-    }
+  public AndFilter(IFilter filter, IFilter filter2) {
+    this.filter = filter;
+    this.filter2 = filter2;
+  }
 
-    @Override
-    public ArrayList<Object> apply(ResultSet rSet) {
-        return null;
-    }
+  @Override
+  public ArrayList<Object> apply(ResultSet rSet) {
+    return null;
+  }
 }
