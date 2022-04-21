7f02668ed64874d0e1cf1d7e1e809add69e8d81f
ngrozdani <64702995+ngrozdani@users.noreply.github.com>
2022-04-21 15:41:58 -0400

    image view id for each member

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/aboutPageController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/aboutPageController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -17,8 +18,33 @@ public class aboutPageController implements Initializable {
   @FXML Label getNickLabel2;
   @FXML AnchorPane bannerAnchor;
 
+  @FXML
+  ImageView raffi;
+  @FXML
+  ImageView carter;
+  @FXML
+  ImageView nikola;
+  @FXML
+  ImageView jack;
+  @FXML
+  ImageView will;
+  @FXML
+  ImageView connor;
+  @FXML
+  ImageView evans;
+  @FXML
+  ImageView cole;
+  @FXML
+  ImageView john;
+  @FXML
+  ImageView owen;
+  @FXML
+  ImageView azu;
+
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+
     bannerAnchor.maxWidthProperty().bind(creditPane.maxHeightProperty());
     //    creditVbox.layoutXProperty().bind(creditPane.maxWidthProperty());
     //    creditVbox.layoutYProperty().bind(creditPane.maxHeightProperty());
