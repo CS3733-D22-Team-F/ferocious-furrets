efc40b11ce63b0710c3cab097ee065da4e423c07
Conner McKevitt <cmmckevitt@wpi.edu>
2022-04-21 20:35:33 -0400

    updated junit tests

--- ProtoypeF/src/test/java/edu/wpi/cs3733/D22/teamF/DAOTester.java
+++ ProtoypeF/src/test/java/edu/wpi/cs3733/D22/teamF/DAOTester.java
@@ -1,13 +1,17 @@
 package edu.wpi.cs3733.D22.teamF;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import edu.wpi.cs3733.D22.teamF.controllers.general.DatabaseManager;
+import edu.wpi.cs3733.D22.teamF.entities.database.ConnType;
+import edu.wpi.cs3733.D22.teamF.entities.location.LocationsDAOImpl;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 
 public class DAOTester {
-  Connection conn = DatabaseManager.getInstance().getDatabaseConnection();
 
   /**
    * Checks that the DAO can get data in the form of an Array List from the DB
@@ -17,23 +21,14 @@ public class DAOTester {
    */
   @Test
   public void testGetData() throws SQLException, IOException {
-    //    LocationsDAOImpl dao = new LocationsDAOImpl();
-    //    dao.initTable("/edu/wpi/cs3733/D22/teamF/csv/TowerLocationsTest.csv");
-    //    assertNotNull(dao.getAllLocationsFromDB());
-    //    System.out.println(dao.getAllLocationsFromDB().get(2).getLongName());
+    DatabaseManager.getInstance().switchConnection(ConnType.EMBEDDED);
+    Connection conn = DatabaseManager.getInstance().getDatabaseConnection();
+    assertTrue(conn.isValid(100));
+    LocationsDAOImpl dao = DatabaseManager.getInstance().getLocationDAO();
+    dao.initTable("/edu/wpi/cs3733/D22/teamF/csv/TowerLocationsTest.csv");
+    assertNotNull(dao.getAllLocationsFromDB());
+    System.out.println(dao.getAllLocationsFromDB().get(2).getLongName());
   }
 
-  /**
-   * Checks that the DAO can get data in the form of an Array List from the DB
-   *
-   * @throws SQLException
-   * @throws IOException
-   */
-  @Test
-  public void testGetDataMedical() throws SQLException, IOException {
-    //    equipmentDAOImpl dao = new equipmentDAOImpl();
-    //    dao.initTable("/edu/wpi/cs3733/D22/teamF/csv/MedEquipTest.csv");
-    //    assertNotNull(dao.getAllEquipment());
-    //    System.out.println(dao.getAllRequests().get(2).getLongName());
-  }
+
 }
