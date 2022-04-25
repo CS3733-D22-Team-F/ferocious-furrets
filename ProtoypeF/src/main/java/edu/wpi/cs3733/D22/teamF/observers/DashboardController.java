52433966020a69dac9fa723e3337e8cecabc4e4c
Will Huang <whuang2146@gmail.com>
2022-04-25 17:35:53 -0400

    added materialfx to filepath

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/observers/DashboardController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/observers/DashboardController.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs3733.D22.teamF.observers;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.ResourceBundle;
 
@@ -8,16 +9,22 @@ import edu.wpi.cs3733.D22.teamF.entities.request.Request;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import io.github.palexdev.materialfx.filter.IntegerFilter;
+import io.github.palexdev.materialfx.filter.StringFilter;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
 
 public class DashboardController implements Initializable {
+
     @FXML BorderPane masterPane;
     @FXML MFXTableView table;
 
   public void initialize(URL location, ResourceBundle resources) {
+
+      initServiceRequestTable();
 
   }
   private void initServiceRequestTable() {
@@ -27,9 +34,25 @@ public class DashboardController implements Initializable {
       MFXTableColumn<Request> getRequesterEmpColumn = new MFXTableColumn<>("Requester Employee ID", true, Comparator.comparing(Request::getRequesterEmpID));
       MFXTableColumn<Request> statusColumn = new MFXTableColumn<>("Status", true, Comparator.comparing(Request::getStatus));
 
-      reqIDColumn.setRowCellFactory(Request -> new MFXTableRowCell<>(Request::getReqID));
+      reqIDColumn.setRowCellFactory(request -> new MFXTableRowCell<>(Request::getReqID));
+      getNodeIDColumn.setRowCellFactory(request -> new MFXTableRowCell<>(Request::getNodeID));
+      getAssignedEmpColumn.setRowCellFactory(request -> new MFXTableRowCell<>(Request::getAssignedEmpID));
+      getRequesterEmpColumn.setRowCellFactory(request -> new MFXTableRowCell<>(Request::getRequesterEmpID));
+      statusColumn.setRowCellFactory(request -> new MFXTableRowCell<>(Request::getStatus));
 
+      table.getTableColumns().addAll(reqIDColumn, getNodeIDColumn, getAssignedEmpColumn, getRequesterEmpColumn, statusColumn);
+      table.getFilters().addAll(
+              new StringFilter<>("RequestID", Request::getReqID),
+              new StringFilter<>("NodeID", Request::getNodeID),
+              new StringFilter<>("Assigned Employee ID", Request::getAssignedEmpID),
+              new StringFilter<>("Requester Employee ID", Request::getRequesterEmpID),
+              new StringFilter<>("Status", Request::getStatus)
+      );
 
+//      table.setItems(); // INSERT OBSERVABLE ARRAYLIST OF ALL REQUEST HERE
+  }
 
+  private ObservableList<Request> getAllRequest() {
+      return null;
   }
 }
