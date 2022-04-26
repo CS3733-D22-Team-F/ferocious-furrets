093ee8050de4b67095ffaea2925a8c83954be418
Nikola Grozdani <64702995+ngrozdani@users.noreply.github.com>
2022-04-26 16:24:36 -0400

    removed unused team member controllers

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/teammemberControllers/AzuController.java
+++
@@ -1,3 +0,0 @@
-package edu.wpi.cs3733.D22.teamF.teammemberControllers;
-
-public class AzuController {}
--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/teammemberControllers/TeamMemberController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/teammemberControllers/TeamMemberController.java
@@ -1,3 +1,4 @@
 package edu.wpi.cs3733.D22.teamF.teammemberControllers;
 
-public class WillController {}
+public class TeamMemberController {
+}
