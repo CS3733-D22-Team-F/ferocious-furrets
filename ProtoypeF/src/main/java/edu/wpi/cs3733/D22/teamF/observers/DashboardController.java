22a859da01faedf4b619b4c017e21adac02c50f4
John Petrarca <64863242+johnmaxpetrarca@users.noreply.github.com>
2022-04-26 23:04:25 -0400

    Dashboard broken

--- ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/observers/DashboardController.java
+++ ProtoypeF/src/main/java/edu/wpi/cs3733/D22/teamF/observers/DashboardController.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.D22.teamF.observers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXNodesList;
 import edu.wpi.cs3733.D22.teamF.controllers.general.DatabaseManager;
 import edu.wpi.cs3733.D22.teamF.entities.request.Request;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
@@ -10,13 +12,22 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
 
 public class DashboardController implements Initializable {
 
@@ -24,10 +35,93 @@ public class DashboardController implements Initializable {
 
   @FXML BorderPane masterPane;
   @FXML MFXTableView table;
+  @FXML Label clock;
+
+  List<Label> cleanLabels = new ArrayList<>();
+  List<Label> dirtyLabels = new ArrayList<>();
+  List<Label> podLabels = new ArrayList<>();
+  List<Label> inUseLabels = new ArrayList<>();
+  List<Label> equipCountLabels = new ArrayList<>();
+
+  static Floor currentFloor = Floor.FL3;
+
+  @FXML JFXNodesList layoutAlerts;
+  @FXML TextField floorSelect;
+
+  @FXML Label cBed;
+  @FXML Label cXRay;
+  @FXML Label cRecliner;
+  @FXML Label cInfusionPump;
+
+  @FXML Label dBed;
+  @FXML Label dXRay;
+  @FXML Label dRecliner;
+  @FXML Label dInfusionPump;
+
+  @FXML Label pBed;
+  @FXML Label pXRay;
+  @FXML Label pRecliner;
+  @FXML Label pInfusionPump;
+
+  @FXML Label iBed;
+  @FXML Label iXRay;
+  @FXML Label iRecliner;
+  @FXML Label iInfusionPump;
+
+  //  @FXML Label ll2Count;
+  //  @FXML Label ll1Count;
+  //  @FXML Label fl1Count;
+  //  @FXML Label fl2Count;
+  //  @FXML Label fl3Count;
+  //  @FXML Label fl4Count;
+  //  @FXML Label fl5Count;
+
+  Time clockWork = new Time(DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalTime.now()));
+  Timeline timeline =
+      new Timeline(
+          new KeyFrame(
+              Duration.seconds(1),
+              event -> {
+                clockWork.onSecondPass();
+                clock.setText(clockWork.getCurrentTime());
+                setLabels();
+              }));
 
   public void initialize(URL location, ResourceBundle resources) {
     updateRequest();
     initServiceRequestTable();
+
+    cleanLabels.add(cBed);
+    cleanLabels.add(cInfusionPump);
+    cleanLabels.add(cRecliner);
+    cleanLabels.add(cXRay);
+
+    dirtyLabels.add(dBed);
+    dirtyLabels.add(dInfusionPump);
+    dirtyLabels.add(dRecliner);
+    dirtyLabels.add(dXRay);
+
+    podLabels.add(pBed);
+    podLabels.add(pInfusionPump);
+    podLabels.add(pRecliner);
+    podLabels.add(pXRay);
+
+    inUseLabels.add(iBed);
+    inUseLabels.add(iInfusionPump);
+    inUseLabels.add(iRecliner);
+    inUseLabels.add(iXRay);
+
+    FloorWatchManager.getInstance()
+        .setLabelsToUpdate(cleanLabels, dirtyLabels, podLabels, inUseLabels);
+    FloorWatchManager.getInstance().setEquipCountLabels(equipCountLabels);
+    setLabels();
+    setAlerts();
+
+    try {
+      FloorObservable.getInstance().setState();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   private void initServiceRequestTable() {
@@ -93,6 +187,82 @@ public class DashboardController implements Initializable {
     } catch (SQLException | IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public void setFloor(Floor floorToSet) {
+    currentFloor = floorToSet;
+  }
+
+  /** Increases the value of the current floor by 1 */
+  public void nextFloor() {
+
+    setFloor(currentFloor.next());
+    // allFloorsObserver.setFloor(currentFloor);
+    //    System.out.println(currentFloor.toInt());
+    setLabels();
+    // System.out.println("Next observer");
+  }
+
+  /** Increases the value of the current floor by 1 */
+  public void prevFloor() {
+    setFloor(currentFloor.prev());
+    //    System.out.println(currentFloor + " for labels");
+    // allFloorsObserver.setFloor(currentFloor);
+    setLabels();
+    //    System.out.println("Prev observer");
+  }
+
+  public void setAlerts() {
+
+    layoutAlerts.getChildren().removeAll();
+
+    List<List<Alert>> allFloorAlerts = AlertObserver.getInstance().getAllFloorAlerts();
+
+    for (List<Alert> floorAlert : allFloorAlerts)
+      for (Alert inFloorAlert : floorAlert) {
+        JFXButton newAlert = new JFXButton(inFloorAlert.getMessage());
+        newAlert.buttonTypeProperty().set(JFXButton.ButtonType.RAISED);
+        layoutAlerts.getChildren().add(new JFXButton(inFloorAlert.getMessage()));
+      }
+  }
+
+  public void readFloorInput() {
+    currentFloor = currentFloor.toFloorEnum(floorSelect.getText());
+    setLabels();
+  }
+
+  /** takes all labels and applies appropriate amount based current observed floor */
+  public void setLabels() {
+
+    List<DashboardObserver> floorObservers = FloorWatchManager.getInstance().getAllFloorObservers();
+    //    System.out.println(currentFloor);
+    //    System.out.println(
+    //        "Current Observer Floor"
+    //            + floorObservers.get(currentFloor.toInt()).getFloor().toFloorString());
+    //    System.out.println(
+    //        currentFloor.toFloorString()
+    //            + " clean List:"
+    //            + floorObservers.get(currentFloor.toInt()).getCleanList().size());
+    //    System.out.println(
+    //        currentFloor.toFloorString()
+    //            + " dirty List:"
+    //            + floorObservers.get(currentFloor.toInt()).getDirtyList().size());
+    //    System.out.println(
+    //        currentFloor.toFloorString()
+    //            + " pod List:"
+    //            + floorObservers.get(currentFloor.toInt()).getPodList().size());
+    //    System.out.println(
+    //        currentFloor.toFloorString()
+    //            + " in-use List:"
+    //            + floorObservers.get(currentFloor.toInt()).getInUse().size());
+
+    List<Alert> currentAlerts = floorObservers.get(currentFloor.toInt()).getFloorAlerts();
+    for (int alert = 0; alert < currentAlerts.size(); alert++)
+      //      System.out.println(currentAlerts.get(alert).getMessage());
+
+      floorObservers.get(currentFloor.toInt()).updateLabels();
+    floorSelect.setText(currentFloor.toFloorString());
+    AlertObserver.getInstance().setFloorAlertCount();
   }
 }
 
